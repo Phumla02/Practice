766d093a0043f97c36d06437e56a0ccbcfd87f1d
phumla02 <phumla02@gmail.com>
2015-04-13 15:11:01 +0200

    max

---
+++ ConsoleApplication2/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	int max;
+	cout << "Enter the first integer \n";
+	cin >> a;
+	cout << "Enter second integer \n";
+	cin >> b;
+	max = a;
+
+	if (b > max)
+		max = b;
+	cout << "Maximum is: \n" << max;
+	system ("pause");
+
+}
